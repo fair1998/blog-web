d6a1d69ab59538610ff31a49ff226873b50b6934
Fair <fair1998@hotmail.com>
2025-11-03 21:26:09 +0700

    add padding to children container in AuthorizedLayout for improved spacing

--- src/app/(authorized)/layout.tsx
+++ src/app/(authorized)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthorizedLayout({ children }: NextAppLayoutProps) {
   return (
     <div className="container mx-auto px-4 min-h-screen relative flex flex-col gap-4">
       <Navbar />
-      <div className="flex-1 flex flex-col">{children}</div>
+      <div className="flex-1 flex flex-col pb-5 xl:pb-10">{children}</div>
     </div>
   );
 }
